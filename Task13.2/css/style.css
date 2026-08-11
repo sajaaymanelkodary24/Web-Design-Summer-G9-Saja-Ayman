ea5c2e1d6984dfd3806e5b39034cbc1a65876550
sajaaymanelkodary24 <157saja2007@gmail.com>
2026-08-11 12:53:23 -0700

    Task13.2

---
+++ Task13.2/css/style.css
@@ -0,0 +1,14 @@
+var items = document.querySelectorAll("li")
+
+items.forEach(function(item) {
+
+    item.onclick = function() {
+
+        items.forEach(function(item) {
+            item.style.color = ""
+        })
+
+        this.style.color = "red"
+    }
+
+})
